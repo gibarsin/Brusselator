cf26ca63bbab8259a5a4daf221b2f0911d07c2db
MatiasComercio <mcomerciovazquez@gmail.com>
2016-09-21 23:15:52 -0300

    Modified Gam-Schmidt

--- src/arnoldi.m
+++ src/arnoldi.m
@@ -2,24 +2,22 @@ function [H, Q]=arnoldi(A, q)
   m = size(A,1);
   % preallocate the memmory so as to save vector columns
   v = zeros(m,m);
-  w = zeros(m,m);
 
   v(:,1) = q/norm(q); % normalize in case it is not
 
   for j=1:m
-    t = A * v(:,j);
-    w(:,j) = t;
+    w = A * v(:,j);
     for i=1:j
       a = v(:,i);
-      h(i,j) = a' * t;
-      w(:,j) = w(:,j) - h(i,j) * a;
+      h(i,j) = a' * w;
+      w = w - h(i,j) * a;
     end
-    aux = norm(w(:,j));
+    aux = norm(w);
     if aux == 0
       break;
     end
     h(j+1,j) = aux;
-    v(:,j+1) = w(:,j)/h(j+1,j);
+    v(:,j+1) = w/h(j+1,j);
   end
 
   Q = v;
