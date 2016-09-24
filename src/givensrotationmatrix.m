793cb6d5f425c183fcfa6b39832b403816461174
Sebastian Kulesz <sebikul@gmail.com>
2016-09-24 14:22:23 -0300

    Implemented Givens Rotation for QR descomposition

---
+++ src/givensrotationmatrix.m
@@ -0,0 +1,31 @@
+# http://www.math.usm.edu/lambers/mat610/sum10/lecture9.pdf
+# page 3
+
+function [c, s]=givensrotationmatrix(a,b)
+
+  if b == 0
+  
+    c = 1
+    s = 0
+  
+  else
+
+    if abs(b) >= abs(a)
+    
+      tau = a / b
+      
+      s = 1 / sqrt(1 + tau**2)
+      c = s * tau
+    
+    else
+    
+      tau = b / a
+      
+      c = 1 / sqrt(1 + tau**2)
+      s =  c * tau
+    
+    end
+
+  end
+
+end
